89cb792866ac91f9e13b7c6be0cac468e0a75826
FacundoChacon <chaconfacu2006@gmail.com>
2026-06-18 18:20:17 -0300

    Añadi la entidad Usuario para probar users, tambien cree Pedido y DetallePedido con sus controladores, servicios y repositorios de cada uno. Tambien cree un DTO carrito el cual va a servir como consulta de cuanto y que se quiere llevar

---
+++ ecomerce/ecomerce/src/main/java/com/bodega/ecomerce/entities/Pedido.java
@@ -0,0 +1,30 @@
+package com.bodega.ecomerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pedido")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @Column(nullable = false)
+    private LocalDateTime fecha;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal total;
+}
